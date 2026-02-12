af98e91b1334c2b1a2031587bfc3c91a840807dc
ashishadarsh1308 <ashishadarsh319@gmail.com>
2026-02-12 21:44:26 +0530

    fixed still page

--- frontend/src/Pages/JobListing.jsx
+++ frontend/src/Pages/JobListing.jsx
@@ -1,10 +1,8 @@
 import React from "react";
 import MainJobSection from "../components/JobListing/MainJobSection";
-import DisclaimerBanner from "../components/Common/DisclaimerBanner";
 function JobListing() {
   return (
     <div className="mt-16">
-      <DisclaimerBanner />
       <MainJobSection />
     </div>
   );
---
+++ frontend/src/Routes/PublicRoute.jsx
@@ -0,0 +1,35 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Navigate } from "react-router-dom";
+import { setLoadingFalse } from "../store/authSlice";
+
+function PublicRoute({ children }) {
+    const { userData, isLoading } = useSelector((store) => store.auth);
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        const timeoutId = setTimeout(() => {
+            if (isLoading) {
+                dispatch(setLoadingFalse());
+            }
+        }, 500);
+
+        return () => clearTimeout(timeoutId);
+    }, [isLoading, dispatch]);
+
+    if (isLoading) {
+        return (
+            <div className="flex justify-center items-center text-gray-600 h-screen">
+                Loading...
+            </div>
+        );
+    }
+
+    if (userData) {
+        return <Navigate to="/" />;
+    }
+
+    return children;
+}
+
+export default PublicRoute;
